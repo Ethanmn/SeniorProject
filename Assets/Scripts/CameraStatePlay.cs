f5e59f2526d10551cde2b7b7019a2f05473a7be2
Ethan Nakashima <ethanmn@hotmail.com>
2015-05-02 16:56:25 -0700

    Tuned up the camera, now much smoother. Used Fixed update

--- Assets/Scripts/CameraStatePlay.cs
+++ Assets/Scripts/CameraStatePlay.cs
@@ -23,20 +23,9 @@ public class CameraStatePlay : I_CameraState {
 		Vector2 pPos = new Vector2(player.transform.position.x, 
 	                               player.transform.position.y);
 		Vector2 cPos = camera.position;
-		Vector2 vel = (pPos - cPos) * 0.25f;
+		Vector2 vel = (pPos - cPos) * 0.33f;
 
 		Vector2 temp = cPos + vel;
-
-		if (Mathf.Abs(temp.x) > Mathf.Abs(pPos.x) ||
-		    (Mathf.Abs(pPos.x) - Mathf.Abs(temp.x)) < 0.08f)
-		{
-			temp.x = pPos.x;
-		}
-		if (Mathf.Abs(temp.y) > Mathf.Abs(pPos.y) ||
-		    (Mathf.Abs(pPos.y) - Mathf.Abs(temp.y)) < 0.08f)
-		{
-			temp.y = pPos.y;
-		}
 
 		camera.position = new Vector3(temp.x, temp.y, -10f);
 
