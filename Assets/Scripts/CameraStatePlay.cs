d05da516a32d4fab3fc71c60b413ba4a65d5f658
Ethan Nakashima <ethanmn@hotmail.com>
2015-05-02 13:25:41 -0700

    Made camera follow non-linear, still is a bit off

--- Assets/Scripts/CameraStatePlay.cs
+++ Assets/Scripts/CameraStatePlay.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class CameraStatePlay : I_CameraState {
 
+	private float speed;
+	private GameObject player;
+
 	void I_CameraState.OnEnter(Transform camera)
 	{
-
+		speed = 0.15f;
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 
 	void I_CameraState.OnExit(Transform camera)
@@ -16,10 +20,26 @@ public class CameraStatePlay : I_CameraState {
 	// Update is called once per frame
 	I_CameraState I_CameraState.Update(Transform camera, float dt)
 	{
-		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		camera.position = new Vector3(player.transform.position.x, 
-		                              player.transform.position.y,
-		                              -10f);
+		Vector2 pPos = new Vector2(player.transform.position.x, 
+	                               player.transform.position.y);
+		Vector2 cPos = camera.position;
+		Vector2 vel = (pPos - cPos) * 0.25f;
+
+		Vector2 temp = cPos + vel;
+
+		if (Mathf.Abs(temp.x) > Mathf.Abs(pPos.x) ||
+		    (Mathf.Abs(pPos.x) - Mathf.Abs(temp.x)) < 0.08f)
+		{
+			temp.x = pPos.x;
+		}
+		if (Mathf.Abs(temp.y) > Mathf.Abs(pPos.y) ||
+		    (Mathf.Abs(pPos.y) - Mathf.Abs(temp.y)) < 0.08f)
+		{
+			temp.y = pPos.y;
+		}
+
+		camera.position = new Vector3(temp.x, temp.y, -10f);
+
 		return null;
 	}
 
